360badbb6a354a2520e1cd9de374d23dacdcc5e4
Nguirlinger <nguirlinger@yahoo.fr>
2026-01-18 18:59:37 +0100

    firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,15 +1,14 @@
+// Initialisation de Firebase
 const firebaseConfig = {
-
   apiKey: "AIzaSyBmry_A6QZkDjHQRsyI4M7ozaDACXxf8OA",
-
   authDomain: "meanset-academy.firebaseapp.com",
-
   projectId: "meanset-academy",
-
   storageBucket: "meanset-academy.firebasestorage.app",
-
   messagingSenderId: "58612733532",
-
   appId: "1:58612733532:web:b11666a284d4f3f57f7899",
-
   measurementId: "G-MMPTLGNP9T"
+};
+
+// Initialiser Firebase
+firebase.initializeApp(firebaseConfig);
+
